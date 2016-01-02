8d8f574b1057e93d8933348f5904e0f98f1f7ff4
whoami <whoami@expressmail.dk>
2016-01-02 16:56:17 +0300

    version 0.1.2 Можно сохранять самому файл с логином паролем и пуком через сам сайт, а можно указывать программе пук, тогда он будет записан в выходной файл. Главное, первым закроется то окно что было первым открыто!

---
+++ cg.py
@@ -0,0 +1,193 @@
+# -*- coding: utf-8 -*-
+__author__ = 'whoami'
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common import exceptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+#Заглушка
+class Logger:
+
+    @staticmethod
+    def store(self, *args):
+        print(args)
+        # pass
+
+
+class CyberGhost(object):
+    def __init__(self, logger, elements, result_text, url):
+        super(CyberGhost, self).__init__()
+        self.logger = Logger() if logger is None else logger
+        self.elements = elements
+        self.result_text = result_text
+        self.plan_text = "Free"
+        self.url = url
+        self.response_timeout = 5
+        self.max_script_run = 5
+        self.page_load_timeout = 20
+        self.impl_wait = 10
+        self.driver = None
+
+    def load_stop(self):
+        self.driver.execute_script('window.stop();')
+
+    def driver_stop(self):
+        self.logger.store("I", "CG", "driver_stop", "Остановка драйвера")
+        self.driver.close()
+
+    def driver_start(self):
+        self.logger.store("I", "CG", "driver_start", "Запуск драйвера...")
+
+        self.fp = webdriver.FirefoxProfile()
+        self.fp.set_preference("http.response.timeout", self.response_timeout)
+        self.fp.set_preference("dom.max_script_run_time", self.max_script_run)
+        self.driver = webdriver.Firefox(firefox_profile=self.fp)
+        self.driver.set_page_load_timeout(self.page_load_timeout)
+        self.driver.implicitly_wait(self.impl_wait)
+        self.driver.get(self.url)
+
+    def login(self, userinfo):
+        user_name, password = userinfo
+        self.logger.store("I", "CG", "login", "Вход на сайт username:%s, "
+                                             "password:%s" % (user_name,
+                                                              password)
+                         )
+        self.driver.get(self.url)
+        login = self.get_element(self.elements["username"])
+        passwd = self.get_element(self.elements["password"])
+        button = self.get_element(self.elements["btn_signin"])
+
+        if not login or not passwd or not button:
+            self.logger.store("E", "CG", "login",
+                             "Не удалось получить элементы!")
+            return False
+
+        login.send_keys(user_name)
+        passwd.send_keys(password)
+
+        try:
+            button.click()
+        except (exceptions.TimeoutException,
+                exceptions.StaleElementReferenceException):
+            self.load_stop()
+
+        if "account" not in self.driver.current_url:
+            self.logger.store("E", "CG", "login", "Вход не удался!!!")
+            return False
+        text = None
+        while True:
+            text = self.get_element(self.elements["login_text"])
+            user_name = user_name.upper()
+            if user_name in text.text:
+                break
+
+        self.load_stop()
+        self.logger.store("I", "CG", "login", "Ок логин: %s" % text.text)
+        return True
+
+    def run_check(self, key):
+        self.logger.store("A", "CG", "run_check", "Чекаем ключ: %s" % key)
+        if not self.__key_input_activate():
+            self.logger.store("E", "CG", "run_check", "__key_input_activate() "
+                                                     "вернул False!")
+            return False
+        if not self.__send_key(key):
+            self.logger.store("E", "CG", "run_check", "__send_key(key) "
+                                                     "вернул False!")
+            return False
+        if self.__get_alert_text():
+            self.logger.store("A", "CG", "run_check",
+                             "Ключ ушспешно активирован")
+            return True
+        else:
+            self.logger.store("E", "CG", "run_check", "__get_alert_text() "
+                                                     "вернул False!")
+            return False
+
+    def __key_input_activate(self):
+        show_form = self.get_element(self.elements['key_input_show'])
+        if show_form:
+            self.logger.store("I", "CG", "__key_input_activate", "Ок")
+            try:
+                show_form.send_keys(Keys.RETURN)
+            except exceptions.StaleElementReferenceException:
+                self.driver.refresh()
+                return self.__key_input_activate()
+            return True
+        else:
+            self.logger.store("E", "CG", "__key_input_activate",
+                             "Не удалось получить key_input_show")
+            return False
+
+    def __send_key(self, key):
+        key_input = self.get_element(self.elements['key_input'])
+        button = self.get_element(self.elements['key_send'])
+        if not button or not key_input:
+            self.logger.store("E", "CG", "__send_key",
+                             "Не удалось получить key_input или key_send")
+            return False
+
+        key_input.clear()
+        key_input.send_keys(key)
+
+        try:
+            button.click()
+        except exceptions.TimeoutException:
+            self.load_stop()
+        self.logger.store("I", "CG", "__send_key", "Элементы получены. Ок")
+        return True
+
+    def __get_alert_text(self):
+        alert_text = self.get_element(self.elements["alert_text"])
+        if not alert_text:
+            self.logger.store("E", "CG", "__get_alert_text",
+                             "Не удалось получить alert_text")
+            return False
+        self.logger.store("I", "CG", "__get_alert_text",
+                         "Алерт текс равен %s" % alert_text.text)
+
+        result = self.result_text["activated"] not in alert_text.text
+        if result:
+            button = self.get_element(self.elements["alert_btn_ok"])
+            if not button:
+                result = False
+                button = self.get_element(self.elements["alert_btn_cancel"])
+        else:
+            button = self.get_element(self.elements["alert_btn_cancel"])
+
+        try:
+            button.click()
+        except exceptions.TimeoutException:
+            self.load_stop()
+
+        return result
+
+    def __load_end(self, msg):
+        while not (msg in self.driver.title):
+            pass
+        return True
+
+    def get_element(self, xpath):
+        try:
+            element = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, xpath))
+            )
+            return element
+        except exceptions.TimeoutException:
+            return None
+
+    def registration(self, userdata):
+        self.driver.get(self.url)
+        login = self.get_element(self.elements["login"])
+        passwd1 = self.get_element(self.elements["passwd"][0])
+        passwd2 = self.get_element(self.elements["passwd"][1])
+        if login and passwd1 and passwd2:
+            login.send_keys(userdata[0])
+            passwd1.send_keys(userdata[1])
+            passwd2.send_keys(userdata[1])
+        else:
+            print("Не удалось получить поля для логина и пароля", userdata)
--- main.py
+++ main.py
@@ -1,21 +1,74 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-from os import sys
+
+from random import choice
+from string import ascii_letters, digits
+import threading
+from cg import CyberGhost
 
 __author__ = 'whoami'
-__version__ = '0.0.0'
+__version__ = '0.1.2'
 __date__ = '02.01.16 13:26'
 __description__ = """
 Helper for regidtration accounts in cyberghostvpn.com
+Первым закроется тот драйвер что был первым открыт!
 """
 
 OUTFILE = "users.txt"
+FROMFILE = "in.txt"
+REGURL = "https://account.cyberghostvpn.com/en_us/create?"
+XPATH = dict(
+    puk=".//*[@id='loginForm']/div[2]/form/input[3]",
+    login=".//*[@id='loginForm']/div[1]/div[2]/form/input[1]",
+    passwd=[".//*[@id='loginForm']/div[1]/div[2]/form/input[2]",
+            ".//*[@id='loginForm']/div[1]/div[2]/form/input[3]", ],
+)
 
 
+def generation_passwd(length):
+    spec = '!@#$%&*?^'
+    all_symbols = digits + ascii_letters
 
-def main(*args):
-    print(args)
+    res = []
+    for _ in range(length):
+        res.append(choice(all_symbols))
+
+    password = ''.join(res)
+    return password
+
+
+def thread(my_func):
+    """
+    Декоратор для запуска функции в потоке
+    :param my_func: functions
+    :return: functions
+    """
+
+    def wrapper(*args, **kwargs):
+        my_thread = threading.Thread(target=my_func, args=args, kwargs=kwargs)
+        my_thread.start()
+
+    return wrapper
+
+
+@thread
+def main():
+    cg = CyberGhost(None, XPATH, None, REGURL)
+    cg.driver_start()
+    login, passwd = generation_passwd(6), generation_passwd(6)
+    cg.registration([login, passwd])
+    puk = input(login + " press enter to close:\n")
+    cg.driver_stop()
+    with open(OUTFILE, 'a') as f:
+        f.writelines(':'.join((login, passwd, puk,)))
+        f.write('\n')
 
 
 if __name__ in "__main__":
-    main(sys.argv)
+    count = int(input("Type count:"))
+    threads_limit = int(input("Count thread:"))
+    threads_limit += threading.active_count()
+    for i in range(count):
+        while threading.active_count() >= threads_limit:
+            pass
+        main()
