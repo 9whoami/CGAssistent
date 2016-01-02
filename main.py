9fa72a125fa051cf2eb116bbfd270094a3ba7898
whoami <whoami@expressmail.dk>
2016-01-02 13:29:53 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from os import sys
+
+__author__ = 'whoami'
+__version__ = '0.0.0'
+__date__ = '02.01.16 13:26'
+__description__ = """
+Helper for regidtration accounts in cyberghostvpn.com
+"""
+
+OUTFILE = "users.txt"
+
+
+
+def main(*args):
+    print(args)
+
+
+if __name__ in "__main__":
+    main(sys.argv)
